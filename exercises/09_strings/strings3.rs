c8be4e9c1c1597d5470d510b917755829dc747fb
Juan-LukeKlopper <jlklopper@protonmail.com>
2024-04-08 20:41:23 +0200

    feat: complete string exercises part 1

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -7,17 +7,38 @@
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let bytes = input.as_bytes();
+
+    let mut x = vec![];
+    for (i, &item) in bytes.iter().enumerate() {
+        if item != b' ' {
+            x.push(&input[i..i+2]);
+        }
+    }
+    x
 }
+
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this!
-    ???
+    input.to_owned() + &String::from(" world!")
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let words = input.split(" ");
+
+    let mut new_words = vec![];
+    for word in words {
+        if word == String::from("cars") {
+            new_words.push(String::from("balloons"))
+        } else {
+            new_words.push(word.to_string())
+        }
+    }
+
+
+    new_words.join(" ")
 }
 
 #[cfg(test)]
